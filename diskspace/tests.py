7f0536820ca909d5760334c726806dbd641533c6
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-06-21 19:17:20 -0300

    criando arquivo tests.py

---
+++ diskspace/tests.py
@@ -0,0 +1,20 @@
+from diskspace import subprocess_check_output
+from diskspace import bytes_to_readable
+from diskspace import print_tree
+from diskspace import calculate_percentage
+from diskspace import du_command
+from diskspace import percentage_args
+
+import unittest
+import os
+import StringIO
+import subprocess
+import sys
+
+
+class DiskspaceTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
