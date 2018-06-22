7cf765e0a01a743f6996882e36bf58239617e72c
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-06-21 23:48:30 -0300

    criado test_subprocess_check_output

--- diskspace/tests.py
+++ diskspace/tests.py
@@ -20,6 +20,12 @@ class DiskspaceTest(unittest.TestCase):
                                'children': [], 'size': self.total_size}
         self.file_tree = {self.path: self.file_tree_node}
 
+    def test_subprocess_check_output(self):
+        cmd = 'du'
+        raw_output = subprocess.check_output(cmd)
+        result = subprocess_check_output(cmd)
+        self.assertEqual(raw_output, result)
+
     def test_bytes_to_readable(self):
         blocks = 64
         result = "32.00Kb"
