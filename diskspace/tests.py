54211931e478382b90ad66c6cbb9319cfa7e4cdf
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-06-21 22:57:32 -0300

    pasta para teste criada e criado test_bytes_to_readable

--- diskspace/tests.py
+++ diskspace/tests.py
@@ -1,9 +1,7 @@
 from diskspace import subprocess_check_output
 from diskspace import bytes_to_readable
 from diskspace import print_tree
-from diskspace import calculate_percentage
-from diskspace import du_command
-from diskspace import percentage_args
+from diskspace import show_space_list
 
 import unittest
 import os
@@ -13,7 +11,19 @@ import sys
 
 
 class DiskspaceTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.path = 'teste'
+        self.largest_size = 6
+        self.total_size = 4
+        self.file_tree_node = {'print_size': '6.00Kb',
+                               'children': [], 'size': self.total_size}
+        self.file_tree = {self.path: self.file_tree_node}
+
+    def test_bytes_to_readable(self):
+        blocks = 64
+        result = "32.00Kb"
+        self.assertEqual(bytes_to_readable(blocks), result)
 
 
 if __name__ == '__main__':
